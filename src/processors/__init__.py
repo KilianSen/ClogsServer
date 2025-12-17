51bbc1a0031be4aa1b403b3cd53e5f20fb6feb12
KilianSen <kilian@senger.group>
2025-12-17 19:22:24 +0100

    Add dynamic processor loading functionality to improve extensibility

--- src/processors/__init__.py
+++ src/processors/__init__.py
@@ -1,5 +1,8 @@
+from importlib import util
+import os
 from abc import ABCMeta, abstractmethod
-from typing import Optional
+from inspect import isabstract
+from typing import Optional, Type
 
 from pydantic import BaseModel
 from sqlmodel import Session
@@ -44,3 +47,27 @@ class Processor[X: BaseModel, Y: BaseModel](BaseModel, metaclass=ABCMeta):
     @abstractmethod
     def on_startup(self):
         pass
+
+
+def load_processors(path) -> list[type[Processor]]:
+    processors: list[Type[Processor]] = []
+
+    for file in os.listdir(path):
+        if not file.endswith(".py"):
+            continue
+        spec = util.spec_from_file_location(file[:-9], os.path.join(path, file))
+        module = util.module_from_spec(spec)
+        e = spec.loader.exec_module(module)
+
+
+        for attr in dir(module):
+            if not isinstance(getattr(module, attr), type):
+                continue
+            if not issubclass(getattr(module, attr), Processor):
+                continue
+
+            if isabstract(getattr(module, attr)):
+                continue
+
+            processors.append(getattr(module, attr))
+    return processors
